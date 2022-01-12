7073e457b85a977c3e486e09d584352d290bb20f
Ed McCreary <edwmccreary@gmail.com>
2022-01-12 00:10:07 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sky"></div>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="4" bgcolor="feffdc"><h1>/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\</h1></marquee>
+      <marquee behavior="" direction="" scrollamount="8" bgcolor="BBEEFF"><h6>,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__</h6></marquee>
+      <marquee behavior="" direction="" scrollamount="10" bgcolor="5DD7FF"><h5>,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__</h5></marquee>
+      <marquee behavior="" direction="" scrollamount="15" bgcolor="5DC5FF"><h4>,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__</h4></marquee>
+      <marquee behavior="" direction="" scrollamount="20" bgcolor="1CAEFF"><h3>,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__</h3></marquee>
+      <marquee behavior="" direction="" scrollamount="25" bgcolor="1C89FF"><h2>,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__</h2></marquee>
+      <marquee behavior="" direction="" scrollamount="30" bgcolor="1C89FF"><h1>,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__,.-*"*-.,__</h1></marquee>
+      
+      <div className="ocean">
+      <br />
+      <br />
+      <br />
+      <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________________________</h1>
+      <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\__/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\</h1>
+      <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</h1>
+      <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/""\________________________/</h1>
+      <br />
+      <br />
+      <marquee behavior="" direction="" scrollamount="8" bgcolor="005F5A"><h6>\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___</h6></marquee>
+      <marquee behavior="" direction="" scrollamount="10" bgcolor="00705D"><h5>\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___</h5></marquee>
+      <marquee behavior="" direction="" scrollamount="15" bgcolor="008363"><h4>\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___</h4></marquee>
+      <marquee behavior="" direction="" scrollamount="20" bgcolor="009956"><h3>\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___</h3></marquee>
+      <marquee behavior="" direction="" scrollamount="25" bgcolor="00B452"><h2>\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___</h2></marquee>
+      <marquee behavior="" direction="" scrollamount="30" bgcolor="00C249"><h1>\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___\___</h1></marquee>
+      <div className="earth"></div>
+      </div>
+      
     </div>
   );
 }
